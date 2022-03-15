9139472034730a5090f5b8d37b8c2d54b268af4d
christian-coop <101524005+christian-coop@users.noreply.github.com>
2022-03-14 20:58:25 -0400

    Add files via upload

--- Lab6.java
+++ Lab6.java
@@ -6,18 +6,31 @@ public class Lab6 {
 
         int userNum1;
         int userNum2;
-        int result;
+        int result = 0;
+        int option;
         Scanner scnr = new Scanner(System.in);
 
+        System.out.println("MENU");
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println();
+        System.out.print("Select an option: ");
+        option = scnr.nextInt();
+
+        System.out.println();
         System.out.print("Please enter the first number: ");
         userNum1 = scnr.nextInt();
         System.out.print("Please enter the second number: ");
         userNum2 = scnr.nextInt();
 
-        result = userNum1 + userNum2;
+        if (option == 1) {
+            result = userNum1 + userNum2;
+        } else if (option == 2) {
+            result = userNum1 - userNum2;
+        }
 
         System.out.println();
-        System.out.print("Your result is: " + result);
+        System.out.println("Your result is: " + result);
 
     }
 }
